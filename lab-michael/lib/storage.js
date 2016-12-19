02f1cdfa46310625ea95af44630f815dd41da540
Michael Bishop <polishthebishop@gmail.com>
2016-12-18 22:21:50 -0800

    Put method not working

--- lab-michael/lib/storage.js
+++ lab-michael/lib/storage.js
@@ -36,6 +36,31 @@ exports.fetchItem = function(recipe, id) {
   })
   .catch(err => Promise.reject(err));
 };
+exports.updateItem = function(recipe, id, newName, newContent, newMealType) {
+  if (!recipe) return Promise.reject(new Error('expected recipe'));
+  if (!id) return Promise.reject(new Error('expected id'));
+  if (!newName) return Promise.reject(new Error('expected new Name'));
+  if (!newContent) return Promise.reject(new Error('expected new Content'));
+  if (!newMealType) return Promise.reject(new Error('expected new MealType'));
+
+
+  return fs.readFileProm(`${__dirname}/../data/${recipe}/${id}.json`)
+  .then(data => {
+    try {
+      let item = JSON.parse(data.toString());
+      item.name = newName;
+      item.content = newContent;
+      item.mealType = newMealType;
+
+      let json = JSON.stringify(item);
+      fs.writeFileProm(`${__dirname}/../data/${recipe}/${id}.json`, json);
+      return item;
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  })
+  .catch(err => Promise.reject(err));
+};
 
 exports.deleteItem = function(recipe, id) {
   return new Promise((resolve,reject) => {
