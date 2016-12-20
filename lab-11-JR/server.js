860002fb9878a8341432440e4fd99ab6b62e473e
Frank Iriarte <jurrish@gmail.com>
2016-12-19 20:48:53 -0800

    cors setup for server to api calls

--- lab-11-JR/server.js
+++ lab-11-JR/server.js
@@ -3,6 +3,13 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app =  express();
+const cors = require('./lib/cors');
+
+app.use(cors);
+
+app.get('/', (req, res) => {
+  res.json({});
+});
 
 app.listen(PORT, function() {
   console.log(`listening on ${PORT}`);
