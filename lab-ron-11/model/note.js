363703de071bc005396b5abc12993f16649907d4
Rubiksron <rubiksron@gmail.com>
2016-12-28 22:36:33 -0800

    i created a new branch to represent lab-12, instead of carry over all of the files into a new dir. I hope this is what is wanted. The tests are not even started, but will be finished soon.

--- lab-ron-11/model/note.js
+++ lab-ron-11/model/note.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const uuid = require('node-uuid');
-const createError = require('http-errors')
+const createError = require('../lib/create-errors.js')
 
 module.exports = function(note, content) {
-  if(!note) throw createError(400, 'note expected.');
-  if(!content) throw createError(400, 'content expected.');
-  this.id = uuid.v1();
-  this.note = note;
-  this.content = content;
+  if(!note) throw createError(400, 'note expected.')
+  if(!content) throw createError(400, 'content expected.')
+  this.id = uuid.v1()
+  this.note = note
+  this.content = content
 };
--- lab-ron-11/route/note-route.js
+++ lab-ron-11/route/note-route.js
@@ -1,35 +1,35 @@
-'use strict';
+'use strict'
 
-const storage = require('../lib/storage.js');
-const Note = require('../model/note.js');
+const storage = require('../lib/storage.js')
+const Note = require('../model/note.js')
 
 
 module.exports = function(router){
 
-  router.get('/api/note', function(req, res) {
-    if (req.query.id) {
-      storage.fetchItem('note', req.query.id)
+  router.get('/api/note/:id', function(req, res) {
+    if (req.params.id) {
+      storage.fetchItem('note', req.params.id)
       .then(note => {
         res.json(note)
       })
       .catch( err => {
         console.error(err)
-        res.status(404).write('error').end()
+        res.status(404).send('error').end()
       })
       return
     } else {
-      if(!req.query.id) {
+      if(!req.params.id) {
         storage.fetchAll('note')
         .then( note => {
           res.json(note)
         })
         .catch(err => {
           console.error(err)
-          res.status(404).write('error').end()
+          res.status(404).send('error').end()
         })
         return
       }
-      res.status(400).write('bad request').end()
+      res.status(400).send('bad request').end()
 
     }
   })
@@ -45,24 +45,24 @@ module.exports = function(router){
   })
 
   router.delete('/api/note', function(req, res){
-    console.log('inside the delete function if statement');
-    if(req.query.id){
-      storage.deleteItem('note', req.query.id)
+    console.log('inside the delete function if statement')
+    if(req.params.id){
+      storage.deleteItem('note', req.params.id)
      .then(() => {
        res.writeHead(204)
        res.end()
      })
      .catch(err => {
        console.error(err)
-       res.write('not found')
+       res.send('not found')
        res.end()
      })
       return
     }
-    res.writeHead(400, {'Content-Type': 'text/plain'})
-    res.write('bad request line63 ')
-    res.end()
-    // res.status(400).write('error').end()
+    // res.writeHead(400, {'Content-Type': 'text/plain'})
+    // res.write('bad request line63 ')
+    // res.end()
+    res.status(400).send('error').end()
 
   })
 }
